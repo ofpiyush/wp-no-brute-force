4e5d2d6657cd7aaf4f9d8f63d6c096c766108392
Piyush <piyush@cio.bz>
2013-04-13 16:50:04 +0530

    Help prevent brute force attacks by setting a limit on login retries

---
+++ wp-no-brute-force.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: No Brute Force
+Plugin URI: http://github.com/CodeitOut/wp-no-brute-force/
+Description: After 10 wrong attempts it forces password reset by failing login to help fight brute force attacks
+Author: Piyush
+Version: 0.1
+Author URI: http://codeitout.com
+*/
+add_filter('check_password', "nobrute_checkpass");
+
+function nobrute_checkpass($check) {
+    $repeated = get_option('nobrute_repeat_pass',0);
+    if(10 <= $repeated) {
+        $check = false;
+    }elseif(false == $check) {
+        update_option('nobrute_repeat_pass',$repeated+1);
+    } else {
+        update_option('nobrute_repeat_pass',0);
+    }
+    return $check;
+}
